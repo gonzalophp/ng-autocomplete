f11d94ff12d55d2b7ff84898264aa1b6241b4307
gonzalo.gradovivero <gonzalo.gradovivero@affiliatewindow.com>
2017-05-01 12:56:49 +0100

    Almost all working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Output, EventEmitter} from '@angular/core';
-import {Subject} from 'rxjs/Rx';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,66 +6,68 @@ import {Subject} from 'rxjs/Rx';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private states = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming',
+  ];
+
   title = 'app works!';
 
-  currentValue= 'c';
+  currentValue = 'c';
 
- private states = [
-      'Alabama',
-      'Alaska',
-      'Arizona',
-      'Arkansas',
-      'California',
-      'Colorado',
-      'Connecticut',
-      'Delaware',
-      'Florida',
-      'Georgia',
-      'Hawaii',
-      'Idaho',
-      'Illinois',
-      'Indiana',
-      'Iowa',
-      'Kansas',
-      'Kentucky',
-      'Louisiana',
-      'Maine',
-      'Maryland',
-      'Massachusetts',
-      'Michigan',
-      'Minnesota',
-      'Mississippi',
-      'Missouri',
-      'Montana',
-      'Nebraska',
-      'Nevada',
-      'New Hampshire',
-      'New Jersey',
-      'New Mexico',
-      'New York',
-      'North Carolina',
-      'North Dakota',
-      'Ohio',
-      'Oklahoma',
-      'Oregon',
-      'Pennsylvania',
-      'Rhode Island',
-      'South Carolina',
-      'South Dakota',
-      'Tennessee',
-      'Texas',
-      'Utah',
-      'Vermont',
-      'Virginia',
-      'Washington',
-      'West Virginia',
-      'Wisconsin',
-      'Wyoming',
-    ];
-
-  constructor() {}
+  constructor() { }
 
   filterStates = (val) => {
+    this.currentValue = val;
     return val ? this.states.filter(s => new RegExp(`${val}`, 'gi').test(s)) : this.states;
   }
 }
--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,75 +1,76 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
-import {Subject} from 'rxjs/Rx';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Rx';
 
 @Component({
-  selector: 'app-autocomplete',
-  templateUrl: './autocomplete.component.html',
-  styleUrls: ['./autocomplete.component.css']
+    selector: 'app-autocomplete',
+    templateUrl: './autocomplete.component.html',
+    styleUrls: ['./autocomplete.component.css']
 })
+
 export class AutocompleteComponent implements OnInit {
 
-  @Input() filterCallback;
-  @Input() value;
+    @Input() filterCallback;
+    @Input() value;
 
-  private frameStart = 0;
-  private filteredList = [];
-  private displayedList = [];
-  private selectedIndex = 0;
-  private settings = {
-    itemsDisplayed: 4,
-    filterDelay: 1000
-  };
-  private valueBehaviourSubject: Subject<string>;
+    private frameStart = 0;
+    private filteredList = [];
+    private displayedList = [];
+    private selectedIndex = 0;
+    private settings = {
+        itemsDisplayed: 4,
+        filterDelay: 1000
+    };
+    private valueSubject: Subject<string>;
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit() {
-    this.valueBehaviourSubject = new Subject<string>();
+    ngOnInit() {
+        this.valueSubject = new Subject<string>();
 
-    this.valueBehaviourSubject
-      .distinctUntilChanged()
-      .debounceTime(this.settings.filterDelay).skip(1)
-      .subscribe(data => this.newInputValue());
-      this.valueBehaviourSubject.next(this.value);
-      this.valueBehaviourSubject.skip(1);
+        this.valueSubject
+            .distinctUntilChanged()
+            .debounceTime(this.settings.filterDelay).skip(1)
+            .subscribe(data => this.newInputValue());
+        this.valueSubject.next(this.value);
+        this.valueSubject.skip(1);
 
-    this.newInputValue();
-  }
+        this.newInputValue();
+    }
 
-  newInputValue() {
-    this.updateFilteredList();
-    this.selectedIndex = 0;
-    this.frameStart = 0;
-    this.updateDisplayedList();
-  }
+    newInputValue() {
+        this.updateFilteredList();
+        this.selectedIndex = 0;
+        this.frameStart = 0;
+        this.updateDisplayedList();
+    }
 
-  onKeyDown(event) {
+    onKeyDown(event) {
 
-      if (event) {
-          if (event.key === 'ArrowUp') {
-              if (this.selectedIndex > 0) {
-                  this.selectedIndex--;
-              }
+        if (event) {
+            if (event.key === 'ArrowUp') {
+                if (this.selectedIndex > 0) {
+                    this.selectedIndex--;
+                }
 
-              if ((this.selectedIndex - this.frameStart ) < 0) {
-                  this.frameStart = this.selectedIndex;
-                  this.updateDisplayedList();
-              }
-          } else if (event.key === 'ArrowDown') {
-              if (this.selectedIndex < (this.filteredList.length - 1)) {
-                  this.selectedIndex++;
-              }
+                if ((this.selectedIndex - this.frameStart) < 0) {
+                    this.frameStart = this.selectedIndex;
+                    this.updateDisplayedList();
+                }
+            } else if (event.key === 'ArrowDown') {
+                if (this.selectedIndex < (this.filteredList.length - 1)) {
+                    this.selectedIndex++;
+                }
 
-              if ((this.selectedIndex - this.frameStart) > this.settings.itemsDisplayed - 1) {
-                  this.frameStart = this.selectedIndex - this.settings.itemsDisplayed + 1;
-                  this.updateDisplayedList();
-              }
-          }
-      }
-  }
+                if ((this.selectedIndex - this.frameStart) > this.settings.itemsDisplayed - 1) {
+                    this.frameStart = this.selectedIndex - this.settings.itemsDisplayed + 1;
+                    this.updateDisplayedList();
+                }
+            }
+        }
+    }
 
     onKeyUp(event) {
-        this.valueBehaviourSubject.next(this.value);
+        this.valueSubject.next(this.value);
     }
 
     updateDisplayedList() {
@@ -79,10 +80,10 @@ export class AutocompleteComponent implements OnInit {
     }
 
     updateFilteredList() {
-      this.filteredList = this.filterCallback(this.value);
+        this.filteredList = this.filterCallback(this.value);
     }
 
-  isHighlighted(i) {
-    return i === (this.selectedIndex - this.frameStart);
-  }
+    isHighlighted(i) {
+        return i === (this.selectedIndex - this.frameStart);
+    }
 }
